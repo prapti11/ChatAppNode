3d180f2cb92d08b31152821bd4d07ab89c1ae83d
Shobhit Agarwal <shobhit.shobhitagarwal.agarwal@gmail.com>
2018-06-22 03:23:26 +0530

    Automatic Scroll

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,22 @@
 var socket=io();
+
+function scrollToBottom(){
+	var messages=jQuery('#messages');
+	var newMessage=messages.children('li:last-child');
+
+	var clientHeight=messages.prop('clientHeight');
+	var scrollTop=messages.prop('scrollTop');
+	var scrollHeight=messages.prop('scrollHeight');
+	var newMessageHeight=newMessage.innerHeight();
+	var lastMessageHeight=newMessage.prev().innerHeight();
+
+	if(clientHeight+scrollTop+newMessageHeight+lastMessageHeight>=scrollHeight)
+	{
+		// console.log('Should Scroll');
+		messages.scrollTop(scrollHeight);
+	}
+}
+
 socket.on('connect',function() {
 	console.log("Connected to Server");
 
@@ -18,6 +36,7 @@ socket.on('newMessage',function(message){
 	 	createdAt : formattedTime
 	 });
 	 jQuery('#messages').append(html);
+	 scrollToBottom();
 	// var formattedTime = moment(message.createdAt).format('MMM Do, YYYY h:mm a');	
 	// var li=jQuery('<li></li>');
 	// li.text(message.from +" "+formattedTime+" : "+message.text);
@@ -35,6 +54,7 @@ socket.on('newLocationMessage',function(message){
 	 	createdAt : formattedTime
 	 });
 	 jQuery('#messages').append(html);
+	 scrollToBottom();
 	// var li=jQuery('<li></li>');
 	// var a=jQuery('<a target="_blank">My current location</a>');
 	// li.text(message.from+" "+formattedTime+":");
