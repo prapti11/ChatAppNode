6591152b77ca3767318f807db91b6844964e9605
Shobhit Agarwal <shobhit.shobhitagarwal.agarwal@gmail.com>
2018-06-21 17:20:58 +0530

    Greet new users and alert others

--- server/server.js
+++ server/server.js
@@ -12,11 +12,17 @@ var io=socketIO(server);
 io.on('connection',function(socket){
 console.log("new user connected");
 
-// socket.emit('newMessage',{
-// 	"from" : "abc",
-// 	"text" : "Hello",
-// 	"createdAt" : "16:66"
-// });
+socket.emit('newMessage',{
+	"from" : "Admin",
+	"text" : "Welcome User",
+	"createdAt" : new Date().getTime()
+});
+
+socket.broadcast.emit('newMessage',{
+	'from' : 'Admin',
+	'text' : 'New user joined',
+	'createdAt' : new Date().getTime()
+});
 
 socket.on('createMessage',function(message){
 console.log("New Messgae Created",message);
@@ -25,6 +31,13 @@ io.emit('newMessage',{
 	text: message.text,
 	createdAt: new Date().getTime()
 });
+// socket.broadcast.emit('newMessage',
+// {
+// 	from: message.from,
+// 	text: message.text,
+// 	createdAt: new Date().getTime()
+// });
+
 });
 
 socket.on('disconnect',function(socket){
