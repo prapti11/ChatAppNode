cc0199c63a551b2f90e584e4d3a451f816998a29
Shobhit Agarwal <shobhit.shobhitagarwal.agarwal@gmail.com>
2018-06-23 00:06:35 +0530

    login signup api added

--- server/server.js
+++ server/server.js
@@ -8,6 +8,24 @@ const port=process.env.PORT||3000;
 const {generateMessage,generateLocationMessage} = require('./utils/message');
 const {isRealString} = require('./utils/validation');
 const {Users} = require('./utils/users');
+const mongoose = require('mongoose');
+const _ = require('lodash');
+const bodyParser = require('body-parser');
+const User= require('../app/models/user');
+
+mongoose.connect('mongodb://shobhit1997:shobhit1997@ds117111.mlab.com:17111/chat_app');
+
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+
+
+app.use(function(req,res,next){
+	res.setHeader('Access-Control-Allow-Origin','*');
+	res.setHeader('Access-Control-Expose-Header','x-auth');
+	res.setHeader('Access-Control-Allow-Header','Origin, X-Requested-With,content-type,Accept');
+	next();
+});
+
 var server=http.createServer(app);
 app.use(express.static(publicPath));
 var io=socketIO(server);
@@ -63,6 +81,41 @@ socket.on('disconnect',function(){
 });
 
 });
+
+
+var router = express.Router();
+
+router.route('/signup')
+	.post(function(req,res){
+
+		var body=_.pick(req.body,['name','phone','password']);
+		var user=new User(body);
+		console.log(body);
+		user.save().then(function(){
+			return user.generateAuthToken();
+		}).then(function(token){
+			res.header('x-auth',token).send(user);
+
+		}).catch(function(e){
+			res.status(400).send(e);
+		});
+	});
+
+router.route('/login')
+	.post(function(req,res){
+		var body=_.pick(req.body,['phone','password']);
+		User.findByCredentials(body.phone,body.password).then(function(user){
+			return user.generateAuthToken().then(function(token){
+				res.header('x-auth',token).send(user);
+			});
+		}).catch(function(e){
+			res.status(400).send(e);
+		});
+
+	});
+
+app.use('/api',router);
+
 
 server.listen(port,function(){
 	console.log("Server running at port "+port);
